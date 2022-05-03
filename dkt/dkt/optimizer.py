ce35c002ac3b523577177a79c245e148db197ef5
h-y-e-j-i <11th_hj@naver.com>
2022-05-03 06:20:15 +0000

    optimizer 추가

--- dkt/dkt/optimizer.py
+++ dkt/dkt/optimizer.py
@@ -1,4 +1,4 @@
-from torch.optim import Adam, AdamW
+from torch.optim import Adam, AdamW, NAdam, Adagrad, Adamax, RAdam, RMSprop, Adadelta, SGD, ASGD, Rprop
 
 
 def get_optimizer(model, args):
@@ -6,7 +6,25 @@ def get_optimizer(model, args):
         optimizer = Adam(model.parameters(), lr=args.lr, weight_decay=0.01)
     if args.optimizer == "adamW":
         optimizer = AdamW(model.parameters(), lr=args.lr, weight_decay=0.01)
-
+    if args.optimizer == "NAdam":
+        optimizer = NAdam(model.parameters(), lr=args.lr, weight_decay=0.01)
+    if args.optimizer == "Adagrad":
+        optimizer = Adagrad(model.parameters(), lr=args.lr, weight_decay=0.01)
+    if args.optimizer == "Adamax":
+        optimizer = Adamax(model.parameters(), lr=args.lr, weight_decay=0.01)
+    if args.optimizer == "RAdam":
+        optimizer = RAdam(model.parameters(), lr=args.lr, weight_decay=0.01)
+    if args.optimizer == "RMSprop":
+        optimizer = RMSprop(model.parameters(), lr=args.lr, weight_decay=0.01)
+    if args.optimizer == "Adadelta":
+        optimizer = Adadelta(model.parameters(), lr=args.lr, weight_decay=0.01)
+    if args.optimizer == "SGD":
+        optimizer = SGD(model.parameters(), lr=args.lr, weight_decay=0.01)
+    if args.optimizer == "ASGD":
+        optimizer = ASGD(model.parameters(), lr=args.lr, weight_decay=0.01)
+    if args.optimizer == "Rprop":
+        optimizer = Rprop(model.parameters(), lr=args.lr)
+        
     # 모든 parameter들의 grad값을 0으로 초기화
     optimizer.zero_grad()
 
